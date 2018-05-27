0604ccd3844b6bae528a86e206819b0713a43263
Nathália Souza <nathalia.dos.s.souza@hotmail.com>
2018-05-27 17:05:21 -0300

    feat: Criação de funções para verificar matchs dos cartões

--- js/app.js
+++ js/app.js
@@ -2,25 +2,20 @@
  * Create a list that holds all of your cards
  */
 const cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+let abertos = [];
+let matchs = 0;
 
 // Função que insere os cards já embaralhados pela função shuffle no DOM
 function inserirCards(){
 	cardsEmbaralhados = shuffle(cards.concat(cards));
-	for (const card of cardsEmbaralhados){
+	for (let card of cardsEmbaralhados){
 		criarCard(card);
 	}
 }
 
 // Função que cria o HTML dos cards
 function criarCard(classeCard){
-	$("ul.deck").append(`<li class="card"><i class="fa ${classeCard}"></i></li>`);
+	$("ul.deck").prepend(`<li class="card"><i class="fa ${classeCard}"></i></li>`);
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -38,18 +33,65 @@ function shuffle(array) {
     return array;
 }
 
+// Função que inicia o jogo
+function iniciarJogo(){
+	inserirCards();
+	$(".card").click(clickCard);
+}
+
+// Função que verifica se o card clicado já está aberto ou possui match
+function clickCard(){
+	let clicado = $(this);
+	if (clicado.hasClass('show') || clicado.hasClass('match')) { 
+        return false; 
+    } else {
+    	if (abertos.length === 0) {
+	        clicado.addClass("open");
+        	clicado.addClass("show");
+        	abertos.push(clicado);
+	    } else if (abertos.length === 1) {
+	        clicado.addClass("open");
+        	clicado.addClass("show");
+        	abertos.push(clicado);
+        	checarMatch();
+	    }
+    }
+}
+
+// Função que checa se os cards são iguais para fazer o match
+function checarMatch(){
+	if (abertos[0].children().attr("class")===abertos[1].children().attr("class")){
+	    console.log("deu match");
+	    setTimeout(match, 500)
+	} else {
+	    console.log("não deu match");
+	    setTimeout(resetAbertos, 500);
+	}
+}
+
+// Variável que armazena função anônima que cria o match
+var match = function(){
+	abertos.forEach(function(card) {
+		card.toggleClass("match");
+	});
+	abertos = [];
+	matchs = matchs + 1;
+
+	if (matchs === 8){
+		console.log("Venceu!");
+	}
+}
+
+// Variável que armazena função anônima que reseta o valor de abertos
+var resetAbertos = function(){
+	abertos.forEach(function(card) {
+		card.toggleClass("open");
+		card.toggleClass("show");
+	});
+	abertos = [];
+}
+
 // Executa ao carregar a página
 $(document).ready(function(){
-    inserirCards();
+    iniciarJogo();
 });
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
